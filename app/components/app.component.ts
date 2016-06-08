fe85d62bba853916d3fd99752d92816f6e459404
Martha Garcia <mgcavero04@hotmail.com>
2016-06-07 21:06:56 -0400

    package.json update

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,17 +1,6 @@
 import { Component } from '@angular/core';
-import { Http } from "@angular/http";
-import 'rxjs/add/operator/map';
-
 @Component({
-    selector: 'my-app',
-    templateUrl: 'components/app.component.html',
-    styleUrls: ['components/app.component.css']
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent {
-    name: string = "Angular 2 on Express";
-    users: {};
-
-    constructor(http: Http) {
-        this.users = http.get("/users").map(data => data.json());
-    }
-}
+export class AppComponent { }
--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,3 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './components/app.component'
-import { Type } from '@angular/core';
-import { HTTP_PROVIDERS } from "@angular/http";
-
-bootstrap(<Type>AppComponent, [
-    HTTP_PROVIDERS
-]);
+import { AppComponent } from './components/app.component';
+bootstrap(AppComponent);
