22c8530a43037383cef1117ddbc330dd9d02b53c
Martha Garcia <mgcavero04@hotmail.com>
2016-06-07 20:57:46 -0400

    make it better

--- public/js/systemjs.config.js
+++ public/js/systemjs.config.js
@@ -30,13 +30,21 @@ var isPublic = typeof window != "undefined";
     'router-deprecated',
     'upgrade'
   ];
-  // Add package entries for angular packages
-  ngPackageNames.forEach(function(pkgName) {
+  // Individual files (~300 requests):
+  function packIndex(pkgName) {
+    packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  }
+  // Bundled (~40 requests):
+  function packUmd(pkgName) {
     packages['@angular/'+pkgName] = { main: pkgName + '.umd.js', defaultExtension: 'js' };
-  });
+  };
+  // Most environments should use UMD; some (Karma) need the individual index files
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
+  // Add package entries for angular packages
+  ngPackageNames.forEach(setPackageConfig);
   var config = {
     map: map,
     packages: packages
-  };
+  }
   System.config(config);
 })(this);
